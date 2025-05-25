471da31409ff571a50855f66f3c2b623cd44fb3b
JaviSrz <ljsuarez7@gmail.com>
2025-05-25 21:58:42 +0200

    Creamos componente para mostrar el titulo de la pagina, con dos input, uno para el texto a mostrar y el otro si está se enviaria un true y si no false para poder hacer cambios de css por ejemplo.

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
 
 type Grade = 'A'|'B'|'F';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './control-flow.component.html',
 })
 export default class ControlFlowComponent {
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   imports: [],
   templateUrl: './title.component.html',
 })
-export class TitleComponent { }
+export class TitleComponent {
+
+  title = input.required<string>();
+  //Con esto si tenemos este input en el llamada a este componente seria true y si no false
+  withShadow = input(false, {transform: booleanAttribute});
+
+}
