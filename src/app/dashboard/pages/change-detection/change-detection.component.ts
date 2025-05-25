b278b98cae807c67d25e5d443b2956fb5d7d8f29
JaviSrz <ljsuarez7@gmail.com>
2025-05-25 22:14:43 +0200

    Ejemplo con el change detection onpush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush, //Revisar que hace esto porque no lo tengo claro del todo
+  imports: [TitleComponent, JsonPipe],
   templateUrl: './change-detection.component.html',
 })
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  };
+
+  public currentFramework = computed(
+    () => `Change Detection - ${this.frameworkAsSignal().name}`
+  );
+
+  constructor() {
+
+    setTimeout(() => {
+
+      // this.frameworkAsProperty.name = 'React';
+
+      //Esta es una forma de actualizar la señal
+      // this.frameworkAsSignal.update(value => ({
+      //   ...value,
+      //   name: 'React'
+      // }));
+
+      //Esta es otra forma de actualizar la señal
+      this.frameworkAsSignal.update(value => {
+        value.name = 'React';
+        return {...value};
+      });
+
+      console.log('Hecho');
+
+    }, 3000);
+
+  }
+
+}
